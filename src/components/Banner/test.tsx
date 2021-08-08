7ae09f2cd489efe1df7cd7dafdda9ac30939c3e6
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-08-08 19:57:55 -0300

    banner implement render tests

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(
+      <Banner
+        img="img test"
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new crashlands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+  })
+})
