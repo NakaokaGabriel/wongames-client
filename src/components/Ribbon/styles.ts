85dde83fecdb5dbedc61029ef95c964cd2383563
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-08-10 21:03:32 -0300

    create ribbon

--- src/components/Ribbon/styles.ts
+++ src/components/Ribbon/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import { darken } from 'polished'
 
 import { RibbonColors, RibbonProps } from '.'
 
@@ -6,16 +7,37 @@ const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]};
     position: relative;
+
+    &::after {
+      border-left-color: ${darken(0.2, theme.colors[color])};
+      border-top-color: ${darken(0.2, theme.colors[color])};
+    }
   `,
 
   normal: (theme: DefaultTheme) => css`
     height: 3.6rem;
     font-size: ${theme.font.sizes.small};
+    padding: 0 ${theme.spacings.small};
+    right: -2rem;
+
+    &::after {
+      top: 3.6rem;
+      border-top-width: 1rem;
+      border-right-width: 2rem;
+    }
   `,
 
   small: (theme: DefaultTheme) => css`
     height: 2.6rem;
     font-size: ${theme.font.sizes.xsmall};
+    padding: 0 ${theme.spacings.xsmall};
+    right: -1.5rem;
+
+    &::after {
+      top: 2.6rem;
+      border-top-width: 0.7rem;
+      border-right-width: 1.5rem;
+    }
   `
 }
 
@@ -23,5 +45,23 @@ export const Wrapper = styled.div<Omit<RibbonProps, 'children'>>`
   ${({ theme, color, size }) => css`
     ${!!size && wrapperModifiers[size](theme)}
     ${!!color && wrapperModifiers.color(theme, color)}
+
+    position: absolute;
+    top: ${theme.spacings.xsmall};
+    display: flex;
+    align-items: center;
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.white};
+
+    &::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      border-style: solid;
+      border-left-width: 0rem;
+      border-right-color: transparent;
+      border-bottom-color: transparent;
+      border-bottom-width: 1rem;
+    }
   `}
 `
