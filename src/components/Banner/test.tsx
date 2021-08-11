00326b6bde6976bb890b09f2cabf17f0a7166f4f
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-08-10 21:26:25 -0300

    integrate banner with ribbon

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -4,17 +4,17 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'img test',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    const { container } = renderWithTheme(
-      <Banner
-        img="img test"
-        title="Defy death"
-        subtitle="<p>Play the new <strong>CrashLands</strong> season"
-        buttonLabel="Buy now"
-        buttonLink="/games/defy-death"
-      />
-    )
+    const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /defy death/i })
@@ -27,5 +27,22 @@ describe('<Banner />', () => {
     expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
   })
 })
