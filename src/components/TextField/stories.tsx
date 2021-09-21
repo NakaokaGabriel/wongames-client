28355bd114545344b225cb3662167ae1e4fba3db
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2021-09-20 22:37:06 -0300

    set up text field component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import TextField, { TextFieldProps } from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'alou',
+    initialValue: 'teste'
+  },
+  argTypes: {
+    onInput: { action: 'changed' }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
---
+++ src/components/TextField/test.tsx
@@ -0,0 +1,59 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import TextField from '.'
+
+describe('<TextField />', () => {
+  it('should render the text field label', () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('should without label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('should render placeholder', () => {
+    renderWithTheme(<TextField placeholder="my placeholder" />)
+
+    expect(screen.getByPlaceholderText('my placeholder')).toBeInTheDocument()
+  })
+
+  it('should dispatch onInput event', async () => {
+    const onInput = jest.fn()
+
+    renderWithTheme(
+      <TextField label="Label" labelFor="Field" id="Field" onInput={onInput} />
+    )
+
+    expect(onInput).not.toHaveBeenCalled()
+
+    const input = screen.getByRole('textbox')
+    const text = 'this is my input field'
+
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toBeCalledTimes(text.length)
+    })
+
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it('should accessible with tab', async () => {
+    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+
+    const input = screen.getByLabelText('Label')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(input).toHaveFocus
+  })
+})
