b45e9d08c11cddec32f986d1b8e7533e93f7728e
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2021-12-12 23:52:16 -0300

    feat(GameDetails): Add genres and rating

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -7,16 +7,22 @@ import * as S from './styles'
 
 type Plataform = 'windows' | 'linux' | 'mac'
 
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
+
 export type GameDeitailsProps = {
   developer: string
   plataforms: Plataform[]
   releaseDate: string
+  rating: Rating
+  genres: string[]
 }
 
 const GameDetails = ({
   developer,
   plataforms,
-  releaseDate
+  releaseDate,
+  rating,
+  genres
 }: GameDeitailsProps) => {
   const plataformIcons = {
     linux: <Linux title="Linux" size={18} />,
@@ -65,12 +71,14 @@ const GameDetails = ({
 
         <S.Block>
           <S.Title>Rating</S.Title>
-          <S.Description>18+</S.Description>
+          <S.Description>
+            {rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}
+          </S.Description>
         </S.Block>
 
         <S.Block>
           <S.Title>Genres</S.Title>
-          <S.Description>Action / Adventure</S.Description>
+          <S.Description>{genres.join(' / ')}</S.Description>
         </S.Block>
       </S.Content>
     </S.Wrapper>
