2033424171346c2809cdda21ea3b0e22bed5ff65
Gabriel Nakaoka <gabrielnakaoka_99@hotmail.com>
2022-01-02 19:28:33 -0300

    feat: create open modal gallery

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+
+import Slider, { SliderSettings } from 'components/Slider'
+
+import * as S from './styles'
+
+export type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const settings: SliderSettings = {
+  arrows: true,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    }
+  ],
+  prevArrow: <ArrowLeft aria-label="previous image" />,
+  nextArrow: <ArrowRight aria-label="next image" />
+}
+
+const Gallery = ({ items }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {items.map((item) => (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            role="button"
+            key={item.src}
+            src={item.src}
+            alt={`Thumb - ${item.label}`}
+            onClick={() => setIsOpen(true)}
+          />
+        ))}
+      </Slider>
+
+      <S.Modal
+        isOpen={isOpen}
+        aria-label="modal"
+        aria-hidden={!isOpen}
+      ></S.Modal>
+    </S.Wrapper>
+  )
+}
+
+export default Gallery
